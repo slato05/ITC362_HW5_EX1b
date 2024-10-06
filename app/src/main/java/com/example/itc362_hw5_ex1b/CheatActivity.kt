39d8cb675d610fceef2dd451ff1d08168acdbc0c
slato05 <slato2001@gmail.com>
2024-10-05 19:45:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/itc362_hw5_ex1b/CheatActivity.kt
@@ -0,0 +1,51 @@
+package com.example.itc362_hw5_ex1b
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.itc362_hw5_ex1b.databinding.ActivityCheatBinding
+
+private const val EXTRA_ANSWER_IS_TRUE = "com.example.itc362_hw5_ex1b.answer_is_true"
+const val EXTRA_ANSWER_SHOWN = "com.example.itc362_hw5_ex1b.answer_shown"
+
+class CheatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCheatBinding
+    private var answerIsTrue = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+
+        binding.showAnswerButton.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            binding.answerTextView.setText(answerText)
+            setAnswerShownResult(true)
+        }
+    }
+
+    private fun setAnswerShownResult(isAnswerShown:Boolean) {
+        val data = Intent().apply {
+
+            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        }
+        setResult(Activity.RESULT_OK, data)
+
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/itc362_hw5_ex1b/Question.kt
@@ -0,0 +1,5 @@
+package com.example.itc362_hw5_ex1b
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
